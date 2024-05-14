b3a5bd861f275151c6ceb364cbe47d5205f54ca0
benpommier <benpommier33@gmail.com>
2024-05-14 14:01:57 +0200

    install

---
+++ src/article/entity/article.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Article {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar' })
+  title: string;
+
+  // Ajoutez dans la table article deux nouvelles propriétés : content et author. Ce sont des chaines de caractères
+  @Column({ type: 'text' })
+  content: string;
+
+  @Column({ type: 'text' })
+  autor: string;
+}
